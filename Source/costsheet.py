42e235cce509addb373022d5a095150eb13efff8
Valeriy Chepelev <valeriychepelev@gmail.com>
2025-03-13 14:54:27 +0300

    costsheet testtable parsing

--- Source/costsheet.py
+++ Source/costsheet.py
@@ -7,6 +7,8 @@ import os
 from colorama import init as colorama_init
 from colorama import Fore
 from colorama import Style
+import re
+import math
 
 
 def define_parser():
@@ -16,6 +18,37 @@ def define_parser():
     parser.add_argument('--debug', default=False, action='store_true',
                         help='logging in debug mode (include tracker and issues info)')
     return parser
+
+
+def import_hr_table(filename):
+    table = pd.read_excel(filename, header=None, index_col=None,
+                          skiprows=9, skipfooter=5)
+    for index, row in table.iterrows():
+        # detect person record by cyrillic in column 2
+        if re.match('[А-ЯЁа-яё \\-]+', str(row[2])):
+            name = ' '.join(str(row[2]).split() +
+                            str(table.loc[index + 1, 1]).split())  # split & join to remove extra spaces
+            # over-check name structure
+            if not re.match('^[А-ЯЁ][а-яё]+(?:-[А-ЯЁ][а-яё]+)*(?:\\s[А-ЯЁ][а-яё]+(?:-[А-ЯЁ][а-яё]+)*){1,2}$',
+                            name):
+                raise ValueError(f'Name not properly formatted: "{name}"')
+            spec = str(table.loc[index + 2, 1]).strip()
+            status = table.loc[index + 1, 9:].tolist()
+            times = row[9:].to_list()
+            times = [0 if math.isnan(t) else t for t in times]  # Zero Nans
+            if len(times) != len(status):
+                raise ImportError(f'Timedata not match status for "{name}"')
+            print(name, spec)
+            print(times)
+            print(status)
+
+
+def import_projects_data():
+    pass
+
+
+def export_sheet():
+    pass
 
 
 def main():
@@ -35,6 +68,8 @@ def main():
     pd.set_option('display.max_rows', 500)
     pd.set_option('display.max_columns', 500)
     pd.set_option('display.width', 1000)
+
+    import_hr_table('TestTable.xlsx')
 
 
 if __name__ == '__main__':
