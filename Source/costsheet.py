b596874904441998f0eb5f76b30848a8bd24219d
ValeriyChepelev <valeriychepelev@gmail.com>
2025-03-15 15:31:19 +0300

    correct persons data

--- Source/costsheet.py
+++ Source/costsheet.py
@@ -56,17 +56,13 @@ def import_hr_table(filename):
             # build dictionary data structure
             person ={'name': name, 'num': emp_num, 'spec': spec}
             person.update({f'h{i}': t for i, t in enumerate(times, 1)})
-            person.update({f'p{i}': t for i, t in enumerate(status, 1)})
+            person.update({f'pres{i}': t for i, t in enumerate(status, 1)})
             persons.append(person)
         elif not (type(row[2]) is float and math.isnan(row[2])):
             # if ceil 2 contain something but not a name
             logging.warning(msg := f'Cell [{index}, 2] contain strange data.')
             print(f'{Fore.RED}WARNING: {msg}{Style.RESET_ALL}')
     return persons
-
-
-def user_match(user: str, name: str):
-    return user.lower() in name.lower()
 
 
 def export_sheet(context):
@@ -105,14 +101,33 @@ def main():
     pp_costs = pd.read_excel(pp_filename, sheet_name='costs', index_col=0)
     boss = pd.read_excel(pp_filename, sheet_name='boss', header=None, index_col=None, usecols=[1])
     print(boss.loc[0, 1], boss.loc[1, 1])
-    print(pp_costs)
+    # print(pp_costs)
 
     # load and parse employee table
     emp_table = pd.DataFrame(import_hr_table(args.employees))
     emp_table.set_index('name', inplace=True)
-    print(emp_table)
+    # print(emp_table)
 
-    # find and update persons names in persons/projects, reindex
+    # find and update persons names in persons/projects
+    # reindex, clear zero persons, sort by name
+    emp_names = emp_table.index.tolist()  # list of full names
+    full_names = list()
+    for name in pp_costs.index.tolist():
+        new_name = [n for n in emp_names if name.lower() in n.lower()]  # get all matching full names
+        if len(new_name) > 1:
+            print(f'{Fore.RED}WARNING: too wide selector for {name}: {"; ".join(new_name)}{Style.RESET_ALL}')
+            logging.warning(f'too wide selector as {name}: {";".join(new_name)}')
+        if len(new_name) == 0:
+            raise ValueError(f'no employee data for {name}')
+        full_names.append(new_name[0])
+    pp_costs['fullname'] = full_names
+    pp_costs.set_index('fullname', inplace=True)  # full names added and reindexed
+    pp_costs['summary'] = pp_costs.sum(axis='columns')  # get summary column
+    pp_costs.drop(pp_costs[pp_costs.summary == 0].index, inplace=True)  # drop persons with zero summary
+    pp_costs.drop('summary', axis='columns', inplace=True)  # delete summary column ???
+    pp_costs.sort_index()  # sort by name
+    print('Original costs data:')
+    print(pp_costs)
 
     # recalculate projects costs according to employee total time
     # (only if total cost > total time)
